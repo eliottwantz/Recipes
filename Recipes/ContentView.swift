e245741d0c251bf0fec128de702a8dccda3c6413
Eliott Wantz <eliott.wantz@develiott.com>
2025-10-08 18:43:58 -0400

    Initial Commit

---
+++ Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipes
+//
+//  Created by Eliott on 2025-10-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Eliott on 2025-10-08.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
